80a390668e744d99b011645fdcbdc859642c2903
madhankumar <madhan2007.psgtech@gmail.com>
2020-03-04 17:50:00 +0530

    first commit

---
+++ src/test/java/com/altimetrik/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik.netflixzuulapigatewayserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
